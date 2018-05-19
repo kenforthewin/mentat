212840ade9014c7fb829680e8f1e69dbb8553837
Kenny <bergquist.kenneth@gmail.com>
2018-05-19 11:01:52 -0400

    Test first cadetship build

--- scripts/run_gcp.sh
+++ scripts/run_gcp.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.gcp.yml up --build -d
+docker network connect nginx_network scalar_app_1
+docker network connect bridge scalar_app_1
+docker restart scalar_app_1
